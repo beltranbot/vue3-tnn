ae79b61abe697439fc41e8470e575277a1e4f8f8
beltranbot <beltranbot@gmail.com>
2021-06-01 21:30:25 -0500

    dynamic/conditional classes

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ const app = Vue.createApp({
                 {
                     title: "name of the wind",
                     author: "patrick rothfuss",
-                    img: "assets/1.jpeg"
+                    img: "assets/1.jpeg",
+                    isFav: true,
                 },
                 {
                     title: "the way of kings",
                     author: "brandon sanderson",
-                    img: "assets/2.jpeg"
+                    img: "assets/2.jpeg",
+                    isFav: false,
                 },
                 {
                     title: "the final empire",
                     author: "Brandon Sanderson",
-                    img: "assets/3.jpeg"
+                    img: "assets/3.jpeg",
+                    isFav: true,
                 }
             ]
         }
@@ -25,6 +28,9 @@ const app = Vue.createApp({
     methods: { // events
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        toggleFav(index) {
+            this.books[index].isFav = !this.books[index].isFav
         }
     }
 })
