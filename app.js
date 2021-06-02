a3a727e7dc6b9cfbca03d43f4bf874c5cb570a15
beltranbot <beltranbot@gmail.com>
2021-06-01 20:47:49 -0500

    conditional rendering

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
+            showBooks: true,
             title: "The final empire",
             author: "Brandon Sanderson",
             age: 45
         }
     },
     methods: { // events
-        changeTitle(title) {
-            this.title = title
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
         }
     }
 })
