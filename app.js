fc5ae527f0a73e02aaf95def97202b17518876bd
beltranbot <beltranbot@gmail.com>
2021-06-01 21:16:56 -0500

    attribute binding

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: "http://www.thenetninja.co.uk",
             showBooks: true,
             books: [
                 {
                     title: "name of the wind",
                     author: "patrick rothfuss",
+                    img: "assets/1.jpeg"
                 },
                 {
                     title: "the way of kings",
                     author: "brandon sanderson",
+                    img: "assets/2.jpeg"
                 },
                 {
                     title: "the final empire",
-                    author: "Brandon Sanderson"
+                    author: "Brandon Sanderson",
+                    img: "assets/3.jpeg"
                 }
             ]
         }
