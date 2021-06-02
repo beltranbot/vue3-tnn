dfa694c8bd21ad21e9f61dba9cbdd197364fd256
beltranbot <beltranbot@gmail.com>
2021-06-01 21:04:00 -0500

    mouse events

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ const app = Vue.createApp({
             showBooks: true,
             title: "The final empire",
             author: "Brandon Sanderson",
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: { // events
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        mouseEnterEvent(event, data) {
+            console.log(event, event.type)
+            if (data) {
+                console.log("data: ", data)
+            }
+            console.log("mouse Entered")
+        },
+        mouseLeaveEvent(event) {
+            console.log("mouse left");
+        },
+        doubleClickEvent(event) {
+            console.log("double clicked!")
+        },
+        handleMouseMove(event) {
+            this.x = event.offsetX
+            this.y = event.offsetY
         }
     }
 })
