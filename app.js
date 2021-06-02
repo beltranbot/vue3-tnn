c95548954b203ddcd0a5fcfc65d2f9e40d4f0853
beltranbot <beltranbot@gmail.com>
2021-06-01 21:09:02 -0500

    v-for

--- app.js
+++ app.js
@@ -2,33 +2,25 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: "The final empire",
-            author: "Brandon Sanderson",
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                {
+                    title: "name of the wind",
+                    author: "patrick rothfuss",
+                },
+                {
+                    title: "the way of kings",
+                    author: "brandon sanderson",
+                },
+                {
+                    title: "the final empire",
+                    author: "Brandon Sanderson"
+                }
+            ]
         }
     },
     methods: { // events
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
-        },
-        mouseEnterEvent(event, data) {
-            console.log(event, event.type)
-            if (data) {
-                console.log("data: ", data)
-            }
-            console.log("mouse Entered")
-        },
-        mouseLeaveEvent(event) {
-            console.log("mouse left");
-        },
-        doubleClickEvent(event) {
-            console.log("double clicked!")
-        },
-        handleMouseMove(event) {
-            this.x = event.offsetX
-            this.y = event.offsetY
         }
     }
 })
